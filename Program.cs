8c369ca3498a040c07330af0676f9cc3c8150070
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:42:45 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace App39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int found = 0;
+            for (int i = text.Length - 1; i >= 0; i--)
+                if (text[i] == text[text.Length - i - 1])
+                {
+                    found++;
+                }
+                else
+                {
+                    break;
+                }
+
+            Console.WriteLine(found);
+        }
+    }
+}
